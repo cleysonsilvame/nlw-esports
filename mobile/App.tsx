b8ad5b6fe845adad3722e13ac53b0bdf501b314e
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-09-16 17:14:52 +0000

    aula 04 - mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,14 @@
-import { StatusBar } from "react-native";
 import {
-  useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
-  Inter_900Black,
-} from '@expo-google-fonts/inter'
+  Inter_900Black, useFonts
+} from '@expo-google-fonts/inter';
+import { StatusBar } from "react-native";
 
-import { Home } from './src/screens/Home';
 import { Background } from "./src/components/Background";
 import { Loading } from "./src/components/Loading";
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +26,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,14 +1,19 @@
-import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
 import { LinearGradient } from "expo-linear-gradient";
+import {
+  ImageBackground, Text, TouchableOpacity,
+  TouchableOpacityProps
+} from "react-native";
 
-import { styles } from './styles';
 import { THEME } from "../../theme";
+import { styles } from "./styles";
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number;
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -18,21 +23,11 @@ interface Props extends TouchableOpacityProps {
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground
-        style={styles.cover}
-        source={data.cover}
-      >
-        <LinearGradient
-          colors={THEME.COLORS.FOOTER}
-          style={styles.footer}
-        >
-          <Text style={styles.name}>
-            {data.name}
-          </Text>
+      <ImageBackground style={styles.cover} source={{ uri: data.bannerUrl }}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.title}</Text>
 
-          <Text style={styles.ads}>
-            {data.ads} anúncios
-          </Text>
+          <Text style={styles.ads}>{data._count.ads} anúncios</Text>
         </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
